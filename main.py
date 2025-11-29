35141555d43170560a61eacbbe15a2b7d196d8a6
Depchai <120307085+depchai72@users.noreply.github.com>
2025-11-29 16:54:46 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -688,6 +688,72 @@ async def wordle(interaction: discord.Interaction):
 
 
 
+@client.tree.command(name="guess_that_flag", description="Đoán lá cờ", guild=GUILD_ID)
+async def flag(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+    countries = {
+    'vn': 'Vietnam', 
+    'ad': 'Andorra', 
+    'ae': 'United Arab Emirates', 
+    'af': 'Afghanistan', 
+    'ag': 'Antigua and Barbuda', 
+    'ai': 'Anguilla', 
+    'al': 'Albania', 
+    'am': 'Armenia', 
+    'ao': 'Angola', 
+    'aq': 'Antarctica', 
+    'ar': 'Argentina', 
+    'as': 'American Samoa', 
+    'at': 'Austria', 
+    'au': 'Australia', 
+    'aw': 'Aruba', 
+    'ax': 'Aland Islands', 
+    'az': 'Azerbaijan', 
+    'ba': 'Bosnia and Herzegovina', 
+    'bb': 'Barbados', 
+    'bd': 'Bangladesh', 
+    'be': 'Belgium', 
+    'bf': 'Burkina Faso', 
+    'bg': 'Bulgaria', 
+    'bh': 'Bahrain', 
+    'bi': 'Burundi', 
+    'bj': 'Benin', 
+    'bl': 'Saint Barthelemy', 
+    'bm': 'Bermuda', #mọe dài quá del sửa hết đc
+    'bn': 'Brunei', 'bo': 'Bolivia', 'bq': 'Caribbean Netherlands', 'br': 'Brazil', 'bs': 'Bahamas', 'bt': 'Bhutan', 'bv': 'Bouvet Island', 'bw': 'Botswana', 'by': 'Belarus', 'bz': 'Belize', 'ca': 'Canada', 'cc': 'Cocos (Keeling) Islands', 'cd': 'DR Congo', 'cf': 'Central African Republic', 'cg': 'Republic of the Congo', 'ch': 'Switzerland', 'ci': "Côte d'Ivoire (Ivory Coast)", 'ck': 'Cook Islands', 'cl': 'Chile', 'cm': 'Cameroon', 'cn': 'China', 'co': 'Colombia', 'cr': 'Costa Rica', 'cu': 'Cuba', 'cv': 'Cape Verde', 'cw': 'Curaçao', 'cx': 'Christmas Island', 'cy': 'Cyprus', 'cz': 'Czechia', 'de': 'Germany', 'dj': 'Djibouti', 'dk': 'Denmark', 'dm': 'Dominica', 'do': 'Dominican Republic', 'dz': 'Algeria', 'ec': 'Ecuador', 'ee': 'Estonia', 'eg': 'Egypt', 'eh': 'Western Sahara', 'er': 'Eritrea', 'es': 'Spain', 'et': 'Ethiopia', 'eu': 'European Union', 'fi': 'Finland', 'fj': 'Fiji', 'fk': 'Falkland Islands', 'fm': 'Micronesia', 'fo': 'Faroe Islands', 'fr': 'France', 'ga': 'Gabon', 'gb': 'United Kingdom', 'gb-eng': 'England', 'gb-nir': 'Northern Ireland', 'gb-sct': 'Scotland', 'gb-wls': 'Wales', 'gd': 'Grenada', 'ge': 'Georgia', 'gf': 'French Guiana', 'gg': 'Guernsey', 'gh': 'Ghana', 'gi': 'Gibraltar', 'gl': 'Greenland', 'gm': 'Gambia', 'gn': 'Guinea', 'gp': 'Guadeloupe', 'gq': 'Equatorial Guinea', 'gr': 'Greece', 'gs': 'South Georgia', 'gt': 'Guatemala', 'gu': 'Guam', 'gw': 'Guinea-Bissau', 'gy': 'Guyana', 'hk': 'Hong Kong', 'hm': 'Heard Island and McDonald Islands', 'hn': 'Honduras', 'hr': 'Croatia', 'ht': 'Haiti', 'hu': 'Hungary', 'id': 'Indonesia', 'ie': 'Ireland', 'il': 'Israel', 'im': 'Isle of Man', 'in': 'India', 'io': 'British Indian Ocean Territory', 'iq': 'Iraq', 'ir': 'Iran', 'is': 'Iceland', 'it': 'Italy', 'je': 'Jersey', 'jm': 'Jamaica', 'jo': 'Jordan', 'jp': 'Japan', 'ke': 'Kenya', 'kg': 'Kyrgyzstan', 'kh': 'Cambodia', 'ki': 'Kiribati', 'km': 'Comoros', 'kn': 'Saint Kitts and Nevis', 'kp': 'North Korea', 'kr': 'South Korea', 'kw': 'Kuwait', 'ky': 'Cayman Islands', 'kz': 'Kazakhstan', 'la': 'Laos', 'lb': 'Lebanon', 'lc': 'Saint Lucia', 'li': 'Liechtenstein', 'lk': 'Sri Lanka', 'lr': 'Liberia', 'ls': 'Lesotho', 'lt': 'Lithuania', 'lu': 'Luxembourg', 'lv': 'Latvia', 'ly': 'Libya', 'ma': 'Morocco', 'mc': 'Monaco', 'md': 'Moldova', 'me': 'Montenegro', 'mf': 'Saint Martin', 'mg': 'Madagascar', 'mh': 'Marshall Islands', 'mk': 'North Macedonia', 'ml': 'Mali', 'mm': 'Myanmar', 'mn': 'Mongolia', 'mo': 'Macau', 'mp': 'Northern Mariana Islands', 'mq': 'Martinique', 'mr': 'Mauritania', 'ms': 'Montserrat', 'mt': 'Malta', 'mu': 'Mauritius', 'mv': 'Maldives', 'mw': 'Malawi', 'mx': 'Mexico', 'my': 'Malaysia', 'mz': 'Mozambique', 'na': 'Namibia', 'nc': 'New Caledonia', 'ne': 'Niger', 'nf': 'Norfolk Island', 'ng': 'Nigeria', 'ni': 'Nicaragua', 'nl': 'Netherlands', 'no': 'Norway', 'np': 'Nepal', 'nr': 'Nauru', 'nu': 'Niue', 'nz': 'New Zealand', 'om': 'Oman', 'pa': 'Panama', 'pe': 'Peru', 'pf': 'French Polynesia', 'pg': 'Papua New Guinea', 'ph': 'Philippines', 'pk': 'Pakistan', 'pl': 'Poland', 'pm': 'Saint Pierre and Miquelon', 'pn': 'Pitcairn Islands', 'pr': 'Puerto Rico', 'ps': 'Palestine', 'pt': 'Portugal', 'pw': 'Palau', 'py': 'Paraguay', 'qa': 'Qatar', 're': 'Réunion', 'ro': 'Romania', 'rs': 'Serbia', 'ru': 'Russia', 'rw': 'Rwanda', 'sa': 'Saudi Arabia', 'sb': 'Solomon Islands', 'sc': 'Seychelles', 'sd': 'Sudan', 'se': 'Sweden', 'sg': 'Singapore', 'sh': 'Saint Helena, Ascension and Tristan da Cunha', 'si': 'Slovenia', 'sj': 'Svalbard and Jan Mayen', 'sk': 'Slovakia', 'sl': 'Sierra Leone', 'sm': 'San Marino', 'sn': 'Senegal', 'so': 'Somalia', 'sr': 'Suriname', 'ss': 'South Sudan', 'st': 'São Tomé and Príncipe', 'sv': 'El Salvador', 'sx': 'Sint Maarten', 'sy': 'Syria', 'sz': 'Eswatini (Swaziland)', 'tc': 'Turks and Caicos Islands', 'td': 'Chad', 'tf': 'French Southern and Antarctic Lands', 'tg': 'Togo', 'th': 'Thailand', 'tj': 'Tajikistan', 'tk': 'Tokelau', 'tl': 'Timor-Leste', 'tm': 'Turkmenistan', 'tn': 'Tunisia', 'to': 'Tonga', 'tr': 'Turkey', 'tt': 'Trinidad and Tobago', 'tv': 'Tuvalu', 'tw': 'Taiwan', 'tz': 'Tanzania', 'ua': 'Ukraine', 'ug': 'Uganda', 'um': 'United States Minor Outlying Islands', 'un': 'United Nations', 'us': 'United States', 'uy': 'Uruguay', 'uz': 'Uzbekistan', 'va': 'Vatican City (Holy See)', 'vc': 'Saint Vincent and the Grenadines', 've': 'Venezuela', 'vg': 'British Virgin Islands', 'vi': 'United States Virgin Islands', 'vu': 'Vanuatu', 'wf': 'Wallis and Futuna', 'ws': 'Samoa', 'xk': 'Kosovo', 'ye': 'Yemen', 'yt': 'Mayotte', 'za': 'South Africa', 'zm': 'Zambia',
+    'zw': 'Zimbabwe'
+    }
+    def check(msg):
+        return msg.author.id == interaction.user.id and msg.channel.id == interaction.channel.id
+    correct = 0
+    wrong = 0
+    i = 0
+    for i in range(5):
+        code, ans = random.choice(list(countries.items()))
+        flag = f"https://flagcdn.com/w1280/{code}.png"
+        r = requests.get(flag)
+        flag_img = io.BytesIO(r.content)
+        if i == 0:
+            await interaction.followup.send('Đây là cờ nước gì?', file=discord.File(flag_img, filename='flag.png'))
+        else:
+            await interaction.channel.send('Đây là cờ nước gì?', file=discord.File(flag_img, filename='flag.png'))            
+
+        msg = await client.wait_for("message", timeout=None, check=check)
+        if msg.content.lower().startswith(ans.lower()) == True:
+            await interaction.channel.send('Chính xác <a:a_tickvang:1422566122305097830>')
+            correct += 1
+        elif msg.content.lower().startswith('idk') or msg.content.lower().startswith('cút') or msg.content.lower().startswith('chịu') or msg.content.lower().startswith('sotp'):
+            await interaction.channel.send(f'Okiiiii😁😁\nĐáp án là: {ans}')
+            return
+        else:
+            await interaction.channel.send('Sai <:cuoiteghe:1432707173892231288>')
+            wrong += 1
+
+    await interaction.channel.send(f'M đã đoán đúng {correct} lần và sai {wrong} lần <:votay:1421701691316895854><:votay:1421701691316895854><:votay:1421701691316895854>')
+    
+
+
+
 import time
 print("🕒 Đang chờ 10 giây trước khi khởi động bot...")
 time.sleep(10)
